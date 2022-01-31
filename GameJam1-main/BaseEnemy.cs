9ca129f4180095a207dcbe125392147e2a5eed2b
Disthename <98347359+Disthename@users.noreply.github.com>
2022-01-31 13:39:26 -0600

    Add files via upload

---
+++ GameJam1-main/BaseEnemy.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class BaseEnemy : RigidBody2D
+{
+	public int Health;
+	public float AttackPower;
+	public float AttackSpeed;
+	public float MovementSpeed = 1.0f;
+	public Navigation2D navigation2D;
+	public Node2D[] NavPoint;
+	public Vector2[] path;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+	// called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		LinearVelocity = GetLocalMousePosition();
+		Position += LinearVelocity.Clamped(100.0f) * delta * MovementSpeed;
+
+		//path = navigation2D.GetSimplePath(NavPoint[0].Position, NavPoint[1].Position, true);
+		//this.LinearVelocity = path[0];
+
+	}
+}
