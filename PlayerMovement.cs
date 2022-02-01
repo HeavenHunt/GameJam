05e52a865f8fe5477ede35633e70ff16acb6a230
Disthename <98347359+Disthename@users.noreply.github.com>
2022-01-31 20:49:13 -0600

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public class PlayerMovement : RigidBody2D
+{
+	[Export] public int speed = 200;
+
+	public Vector2 newVelocity = new Vector2();
+
+	public void GetInput()
+	{
+		LookAt(GetGlobalMousePosition());
+		newVelocity = new Vector2();
+
+		if (Input.IsActionPressed("right"))
+			newVelocity.x += 1;
+
+		if (Input.IsActionPressed("left"))
+			newVelocity.x -= 1;
+
+		if (Input.IsActionPressed("down"))
+			newVelocity.y += 1;
+
+		if (Input.IsActionPressed("up"))
+			newVelocity.y -= 1;
+
+		newVelocity = newVelocity.Normalized() * speed;
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		GetInput();
+		this.LinearVelocity = newVelocity;
+	}
+}
