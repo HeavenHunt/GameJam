be484eaf35533ffbedb282c10988dd50f8be41d8
johnnybboy <92316790+johnnybboy@users.noreply.github.com>
2022-02-04 15:25:47 -0600

    KeyUI Working, need to add texture changing

--- scripts/UIController.cs
+++ scripts/UIController.cs
@@ -5,13 +5,14 @@ public class UIController : Control
 {
     private TextureProgress healthProgress;
     private HBoxContainer keyContainer;
+    private PackedScene keyScene;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         healthProgress = GetNode<TextureProgress>("UI_Container/HealthBar/HealthProgress");
         keyContainer = GetNode<HBoxContainer>("UI_Container/KeyContainer");
-
+        keyScene = (PackedScene)GD.Load("res://scenes/Keys/Key_UI.tscn");
     }
 
     public void _on_Player_UpdateHealth(float healthCurrent, float healthMax){
@@ -20,19 +21,33 @@ public class UIController : Control
     }
 
     public void _on_Player_AddKey(Node key){
-        //index 2 should be the last node of a Key scene, hopefully :T
+        //print pickup
         GD.Print("Picked up " + key.Name + "!");
-        TextureRect keyTexture = key.GetChild<TextureRect>(key.GetChildCount()-1);
-        TextureRect keyToAdd = new TextureRect();
-        keyToAdd.Texture = keyTexture.Texture;
-        GD.Print("Target: " + keyTexture.GetRect());
-        GD.Print("1: " + keyToAdd.GetRect());
-        //keyToAdd.Expand = true;
-        keyToAdd.RectSize = new Vector2(32, 32);
-        GD.Print("2: " + keyToAdd.GetRect());
+
+        //create new Key_UI
+        TextureRect keyToAdd = (TextureRect)keyScene.Instance();
+
+        //set proper Texture
+        if (key.IsInGroup("Key_Blue")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Keys_Blue.png");
+        }
+        else if (key.IsInGroup("Key_Green")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Key_Green.png");
+        }
+        else if (key.IsInGroup("Key_Purple")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Key_Purple.png");
+        }
+        else if (key.IsInGroup("Key_Red")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Key_Red.png");
+        }
+        else if (key.IsInGroup("Key_Teal")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Key_Teal.png");
+        }
+        else if (key.IsInGroup("Key_Yellow")){
+            keyToAdd.Texture = (Texture)GD.Load("res://assets/art/Key_Yellow.png");
+        }
+        
+        
         keyContainer.AddChild(keyToAdd);
-        GD.Print("3: " + keyToAdd.GetRect());
-        keyToAdd.RectSize = new Vector2(32, 32);
-        GD.Print("4: " + keyToAdd.GetRect());
     }
 }
