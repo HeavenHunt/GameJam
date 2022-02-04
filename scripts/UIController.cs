4189ea095f811fb12e5584c4f5771d08c29106eb
johnnybboy <brantonj2795@my.uwstout.edu>
2022-02-04 10:20:01 -0600

    Added new UI visuals

--- scripts/UIController.cs
+++ scripts/UIController.cs
@@ -3,12 +3,12 @@ using System;
 
 public class UIController : Control
 {
-    private ProgressBar healthProgress;
+    private TextureProgress healthProgress;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        healthProgress = GetNode<ProgressBar>("/root/World/Control/UI_Container/HealthBar/HealthProgress");
+        healthProgress = GetNode<TextureProgress>("/root/World/Control/UI_Container/HealthBar/HealthProgress");
     }
 
     public void _on_Player_UpdateHealth(float healthCurrent, float healthMax){
