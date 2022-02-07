8743e0d19d29984bed1bad3c8eec3204f05dad6c
Logan <pribyll2420@my.uwstout.edu>
2022-02-07 14:23:00 -0600

    Edits

--- Scripts/PauseMenu.cs
+++ Scripts/PauseMenu.cs
@@ -3,30 +3,37 @@ using System;
 
 public class PauseMenu : Control
 {
-    private bool isPaused = false;
+	private bool isPaused = false;
 
-    public bool GetIsPaused() {
-        return isPaused;
-    }
+	public bool GetIsPaused() {
+		return isPaused;
+	}
 
-    public void SetIsPaused(bool value) {
-        isPaused = value;
-        GetTree().Paused = isPaused; 
-        Visible = isPaused;
-    }
+	public void SetIsPaused(bool value) {
+		isPaused = value;
+		GetTree().Paused = isPaused; 
+		Visible = isPaused;
+	}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("pause")) {
-            SetIsPaused(!GetIsPaused());
-        }
-    }
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("pause")) {
+			SetIsPaused(!GetIsPaused());
+		}
+	}
 
-    public void Resume_Button_Pressed() {
-        SetIsPaused(false);
-    }
+	public void Resume_Button_Pressed() {
+		SetIsPaused(false);
+	}
+	
+	public void Restart_Button_Pressed() {
+	 GetTree().
+	}
 
-    public void Quit_Button_Pressed() {
-        GetTree().Quit();
-    }
+	public void Quit_Button_Pressed() {
+		GetTree().Quit();
+	}
 }
+
+
+
