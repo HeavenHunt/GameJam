b80922ce765ada3ab4076e4bd793646a4176b120
Logan <pribyll2420@my.uwstout.edu>
2022-02-09 14:23:40 -0600

    Finished Start Menu and added credits

--- Scripts/StartMenu.cs
+++ Scripts/StartMenu.cs
@@ -3,10 +3,25 @@ using System;
 
 public class StartMenu : Control
 {
+    Control Credits;
+    bool VisibleCredits;
+
+    public override void _Ready()
+    {
+        Credits = GetNode<Control>("/root/StartMenu/Credits");
+        VisibleCredits = false;
+    }
     public void Start_Button_Pressed() {
         GetTree().ChangeScene("res://Scenes/World.tscn");
     }
     public void Quit_Button_Pressed() {
 		GetTree().Quit();
 	}
+    public void Credits_Button_Pressed() {
+        Credits.Visible = true;
+    }
+
+    public void Back_Button_Pressed() {
+        Credits.Visible = false;
+    }
 }
